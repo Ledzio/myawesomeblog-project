2379071f03a7b9de60038a3cfa3e38f5d0b28664
Ledzio <osalamaha1@gmail.com>
2022-07-07 23:52:18 +0300

    Add PostgreSQL, events, new superuser

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,6 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    ''
+    'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'myawesomeblog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'myawesomeblogdb',
+        'USER':'postgres',
+        'PASSWORD':'021011ua',
+        'HOST': 'localhost',
+        'PORT':'5432'
     }
 }
 
@@ -121,3 +127,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT=BASE_DIR / 'media'
+MEDIA_URL='/awesomemedia/'
