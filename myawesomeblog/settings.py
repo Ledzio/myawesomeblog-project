806c1d7164578a00c5c2f2c5555beab463df6c4d
Ledzio <osalamaha1@gmail.com>
2022-07-08 01:34:38 +0300

    Added blog.app connected to admin

--- myawesomeblog/settings.py
+++ myawesomeblog/settings.py
@@ -31,7 +31,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    ''
+    
+    'blog.apps.BlogConfig',
     'events.apps.EventsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
